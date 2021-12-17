de0fde513065f42b76a4579bc86cd465e2605046
letientai <82451807+letientai@users.noreply.github.com>
2021-12-17 20:27:40 +0700

    Update cart.jsx

--- src/pages/cart/cart.jsx
+++ src/pages/cart/cart.jsx
@@ -7,7 +7,7 @@ import IconButton from "@mui/material/IconButton";
 import DeleteIcon from "@mui/icons-material/Delete";
 import "./cart.scss";
 import dataCart from "../../assets/data/dataCart";
-import { Prev } from "react-bootstrap/esm/PageItem";
+
 function Cart() {
   const [loadingPage, setLoadingPage] = useState(true);
   const [toTal, setToTalPayMent] = useState();
